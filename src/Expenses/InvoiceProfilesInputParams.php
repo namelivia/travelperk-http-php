73458020583df9df900bad3deede67c93697ed5e
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-09-27 19:47:42 +0200

    Add invoice profiles query

---
+++ src/Expenses/InvoiceProfilesInputParams.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Namelivia\TravelPerk\Expenses;
+
+class InvoiceProfilesInputParams
+{
+    private $offset;
+    private $limit;
+
+    public function setOffset(int $offset)
+    {
+        $this->offset = $offset;
+        return $this;
+    }
+
+    public function setLimit(int $limit)
+    {
+        $this->limit = $limit;
+        return $this;
+    }
+
+    public function asUrlParam()
+    {
+        return http_build_query([
+            'offset' => $this->offset,
+            'limit'  => $this->limit,
+        ]);
+    }
+}
