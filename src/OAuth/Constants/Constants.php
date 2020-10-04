e78b940d3a017acf3a2b3a1dd7e21dc786c2118d
José Ignacio Amelivia Santiago <nacho.amelivia@travelperk.com>
2020-10-04 20:30:05 +0200

    Fix a couple of small issues

--- src/OAuth/Constants/Constants.php
+++ src/OAuth/Constants/Constants.php
@@ -6,6 +6,6 @@ namespace Namelivia\TravelPerk\OAuth\Constants;
 
 class Constants
 {
-    const TOKEN_URL = 'https://app.travelperk.com/accounts/oauth2/token';
-    const AUTHORIZE_URL = 'https://app.travelperk.com/oauth2/authorize';
+    const TOKEN_URL = 'https://app.travelperk.com/accounts/oauth2/token/';
+    const AUTHORIZE_URL = 'https://app.travelperk.com/oauth2/authorize/';
 }
